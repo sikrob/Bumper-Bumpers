bea260d859b63bff315cdd8930ce809dd3070336
Robert Sikorski <sikorski.rob@gmail.com>
2019-03-24 17:44:03 -0400

    Initial Commit

---
+++ Bumper Bumpers/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Bumper Bumpers
+//
+//  Created by Robert Sikorski on 3/24/19.
+//  Copyright © 2019 Robert Sikorski. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
