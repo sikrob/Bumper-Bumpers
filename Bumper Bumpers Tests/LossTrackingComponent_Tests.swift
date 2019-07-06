ddaf7363853e89116dbba0370cc667d7711d320a
Robert Sikorski <sikorski.rob@gmail.com>
2019-07-06 12:03:16 -0400

    test LossTracking logic

---
+++ Bumper Bumpers Tests/LossTrackingComponent_Tests.swift
@@ -0,0 +1,42 @@
+//
+//  LossTrackingComponent_Tests.swift
+//  Bumper Bumpers Tests
+//
+//  Created by Robert Sikorski on 7/6/19.
+//  Copyright © 2019 Robert Sikorski. All rights reserved.
+//
+
+import XCTest
+import SpriteKit
+@testable import Bumper_Bumpers
+
+class LossTrackingComponent_Tests: XCTestCase {
+  var targetShape: SKShapeNode!
+  var targeterShape: SKShapeNode!
+
+  override func setUp() {
+    super.setUp()
+    targetShape = SKShapeNode(circleOfRadius: 10)
+    targeterShape = SKShapeNode(circleOfRadius: 10)
+  }
+
+  override func tearDown() {
+    super.tearDown()
+    targetShape = nil
+    targeterShape = nil
+  }
+
+  func testLossTrackingComponentsCanInitWithNonLoser() {
+    targetShape.position = CGPoint(x: 0, y: 0)
+    targeterShape.position = CGPoint(x: 0, y: 0)
+    let lossTrackingComponent = LossTrackingComponent(withTargetShape: targetShape, targeterShape: targeterShape)
+    XCTAssert(!lossTrackingComponent.loser)
+  }
+
+  func testLossTrackingComponentsCanInitWithLoser() {
+    targetShape.position = CGPoint(x: 0, y: 0)
+    targeterShape.position = CGPoint(x: 100, y: 0)
+    let lossTrackingComponent = LossTrackingComponent(withTargetShape: targetShape, targeterShape: targeterShape)
+    XCTAssert(lossTrackingComponent.loser)
+  }
+}
---
+++ Bumper Bumpers Tests/LossTrackingSystem_Tests.swift
@@ -0,0 +1,72 @@
+//
+//  LossTrackingSystem_Tests.swift
+//  Bumper Bumpers Tests
+//
+//  Created by Robert Sikorski on 7/6/19.
+//  Copyright © 2019 Robert Sikorski. All rights reserved.
+//
+
+import XCTest
+import SpriteKit
+@testable import Bumper_Bumpers
+
+
+class LossTrackingSystem_Tests: XCTestCase {
+  var targetShape: SKShapeNode!
+  var targeterShapeA: SKShapeNode!
+  var targeterShapeB: SKShapeNode!
+  var lossTrackingComponentA: LossTrackingComponent!
+  var lossTrackingComponentB: LossTrackingComponent!
+  var lossTrackingSystem: LossTrackingSystem!
+
+  override func setUp() {
+    super.setUp()
+
+    targetShape = SKShapeNode(circleOfRadius: 10)
+    targeterShapeA = SKShapeNode(circleOfRadius: 10)
+    targeterShapeA.name = "ShapeA"
+    targeterShapeB = SKShapeNode(circleOfRadius: 10)
+    targeterShapeB.name = "ShapeB"
+    lossTrackingComponentA = LossTrackingComponent(withTargetShape: targetShape, targeterShape: targeterShapeA)
+    lossTrackingComponentB = LossTrackingComponent(withTargetShape: targetShape, targeterShape: targeterShapeB)
+    lossTrackingSystem = LossTrackingSystem()
+    lossTrackingSystem.addComponent(lossTrackingComponentA)
+    lossTrackingSystem.addComponent(lossTrackingComponentB)
+  }
+
+  override func tearDown() {
+    super.tearDown()
+    lossTrackingComponentB = nil
+    lossTrackingComponentA = nil
+    targeterShapeB = nil
+    targeterShapeA = nil
+    targetShape = nil
+  }
+
+  func testLossTrackSystemReturnNilWhenNoLosers() {
+    targetShape.position = CGPoint(x: 0, y: 0)
+    targeterShapeA.position = CGPoint(x: 0, y: 0)
+    targeterShapeB.position = CGPoint(x: 0, y: 0)
+    lossTrackingSystem.update()
+    let winner: String? = lossTrackingSystem.findWinner()
+    XCTAssertNil(winner)
+  }
+
+  func testLossTrackSystemReturnNilWhenOnlyLosers() {
+    targetShape.position = CGPoint(x: 0, y: 0)
+    targeterShapeA.position = CGPoint(x: 100, y: 0)
+    targeterShapeB.position = CGPoint(x: 100, y: 0)
+    lossTrackingSystem.update()
+    let winner: String? = lossTrackingSystem.findWinner()
+    XCTAssertNil(winner)
+  }
+
+  func testLossTrackSystemReturnWinnerNameIfWinner() {
+    targetShape.position = CGPoint(x: 0, y: 0)
+    targeterShapeA.position = CGPoint(x: 0, y: 0)
+    targeterShapeB.position = CGPoint(x: 100, y: 0)
+    lossTrackingSystem.update()
+    let winner: String? = lossTrackingSystem.findWinner()
+    XCTAssert(winner == targeterShapeA.name)
+  }
+}
