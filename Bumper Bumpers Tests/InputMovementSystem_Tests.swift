d4f5947355f1b66aec4f28e57c6e0156184f6836
Robert Sikorski <sikorski.rob@gmail.com>
2019-07-08 23:49:49 -0400

    setup IMS_Tests file

---
+++ Bumper Bumpers Tests/InputMovementSystem_Tests.swift
@@ -0,0 +1,29 @@
+//
+//  InputMovementSystem_Tests.swift
+//  Bumper Bumpers Tests
+//
+//  Created by Robert Sikorski on 7/8/19.
+//  Copyright © 2019 Robert Sikorski. All rights reserved.
+//
+
+import GameplayKit
+import XCTest
+@testable import Bumper_Bumpers
+
+class InputMovementSystem_Tests: XCTestCase {
+  override func setUp() {
+    super.setUp()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+  }
+
+  func testCurrentActiveDirectionsReturnsAccurateDictionary() {
+    XCTAssert(false)
+  }
+
+  func testUpdatePhysicsBodyVelocityUpdatesComponentPhysicsBodyAsSideEffect() {
+    XCTAssert(false)
+  }
+}
