9f58954fc1a6a4e67e9d7d07ec12aeb11a96b14a
Robert Sikorski <sikorski.rob@gmail.com>
2019-07-08 23:36:53 -0400

    tests for clickCheckSystem

--- Bumper Bumpers Tests/ClickCheckSystem_Tests.swift
+++ Bumper Bumpers Tests/ClickCheckSystem_Tests.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2019 Robert Sikorski. All rights reserved.
 //
 
-import Foundation
+import GameplayKit
+import XCTest
+@testable import Bumper_Bumpers
+
+class ClickCheckSystem_Tests: XCTestCase {
+  var clickableNode: SKShapeNode!
+  var clickableComponent: ClickableComponent!
+  var clickCheckSystem: ClickCheckSystem!
+
+  override func setUp() {
+    super.setUp()
+    clickableNode = SKShapeNode(circleOfRadius: 10)
+    clickableComponent = ClickableComponent(withClickableNode: clickableNode, clickMode: .mouseUp)
+    clickCheckSystem = ClickCheckSystem()
+    clickCheckSystem.addComponent(clickableComponent)
+  }
+
+  override func tearDown() {
+    super.tearDown()
+    clickableNode = nil
+    clickableComponent = nil
+    clickCheckSystem = nil
+  }
+
+  func testUpdateChangesClickStatus() {
+    clickableNode.position = CGPoint(x: 0, y: 0)
+    let insideClick = CGPoint(x: 0, y: 0)
+    XCTAssert(!clickableComponent.clicking)
+    clickCheckSystem.update(at: insideClick, clickMode: .mouseUp)
+    XCTAssert(clickableComponent.clicking)
+  }
+}
