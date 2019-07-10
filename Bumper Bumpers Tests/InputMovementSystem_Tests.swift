508d5cc6e3ad8948c320c719ab0b7786fc0743d3
Robert Sikorski <sikorski.rob@gmail.com>
2019-07-09 23:19:10 -0400

    tests for inputmovementsystem

--- Bumper Bumpers Tests/InputMovementSystem_Tests.swift
+++ Bumper Bumpers Tests/InputMovementSystem_Tests.swift
@@ -11,19 +11,32 @@ import XCTest
 @testable import Bumper_Bumpers
 
 class InputMovementSystem_Tests: XCTestCase {
+  var inputMovementComponent: InputMovementComponent!
+  var inputMovementSystem: InputMovementSystem!
+
   override func setUp() {
     super.setUp()
+    let physicsBody = SKPhysicsBody(circleOfRadius: 10)
+    inputMovementComponent = InputMovementComponent(with: physicsBody, baseMovementAcceleration: 10, inputDirections: [0:"DOWN", 1:"UP"])
+    inputMovementSystem = InputMovementSystem()
+    inputMovementSystem.addComponent(inputMovementComponent)
   }
 
   override func tearDown() {
     super.tearDown()
+    inputMovementComponent = nil
+    inputMovementSystem = nil
   }
 
-  func testCurrentActiveDirectionsReturnsAccurateDictionary() {
-    XCTAssert(false)
+  func testUpdateActiveDirectionsAccuratelyUpdates() {
+    inputMovementSystem.updateActiveDirections(fromCodes: [1], component: inputMovementComponent)
+    XCTAssert(!inputMovementComponent.activeDirections["DOWN"]!)
+    XCTAssert(inputMovementComponent.activeDirections["UP"]!)
   }
 
-  func testUpdatePhysicsBodyVelocityUpdatesComponentPhysicsBodyAsSideEffect() {
-    XCTAssert(false)
+  func testUpdatePhysicsBodyVelocityUpdatesComponentPhysicsBody() {
+    inputMovementComponent.activeDirections.updateValue(true, forKey: "UP")
+    inputMovementSystem.updatePhysicsBodyVelocity(forComponent: inputMovementComponent)
+    XCTAssert(Int(inputMovementComponent.physicsBody.velocity.dy) == 10)
   }
 }
