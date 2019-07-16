86e7350e2c01aebf09234926aae2794bc004cd0a
Robert Sikorski <sikorski.rob@gmail.com>
2019-07-16 00:24:33 -0400

    skeleton for PlayWinState

--- Bumper Bumpers/States/PlaySceneStates/PlayWinState.swift
+++ Bumper Bumpers/States/PlaySceneStates/PlayWinState.swift
@@ -9,5 +9,21 @@
 import GameplayKit
 
 class PlayWinState: GKState {
+  private let clickCheckSystem: ClickCheckSystem
+  private let lossTrackingSystem: LossTrackingSystem
 
+  init(clickCheckSystem: ClickCheckSystem, lossTrackingSystem: LossTrackingSystem) {
+    self.clickCheckSystem = clickCheckSystem
+    self.lossTrackingSystem = lossTrackingSystem
+    super.init()
+  }
+
+  override func update(deltaTime seconds: TimeInterval) {
+    update(clickLocation: nil)
+  }
+
+  func update(clickLocation: CGPoint?) {
+    // update click
+    // act on results
+  }
 }
